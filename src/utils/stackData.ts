134b066d9cac85a2e4b99d72f67d84a0a940923d
ismaelSS <ismael1337sn@gmail.com>
2023-05-25 20:54:37 -0300

    add: tachs prisma & nest.js

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,5 +1,5 @@
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript, SiSqlite, SiInsomnia } from "react-icons/si";
+import { SiPostgresql, SiTypescript, SiSqlite, SiInsomnia, SiNestjs, SiPrisma } from "react-icons/si";
 import { FaGit, FaPython } from "react-icons/fa";
 import {
   FaDocker,
@@ -12,33 +12,20 @@ import {
 import {DiDjango} from "react-icons/di"
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
+  { title: "HTML", img: FaHtml5 },
+  { title: "CSS", img: FaCss3Alt },
+  { title: "JS", img: FaJs },
   { title: "TypeScript", img: SiTypescript },
   { title: "Python", img: FaPython },
   { title: "Git", img: FaGit },
-  {
-    title: "React",
-    img: FaReact,
-  },
+  { title: "React",img: FaReact },
   { title: "Next.js", img: TbBrandNextjs },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
+  { title: "Node JS", img: FaNode },
+  { title: 'Nest js',img: SiNestjs },
   { title: "django", img:DiDjango },
   { title: "Docker", img: FaDocker },
+  { title: "prisma", img: SiPrisma },
   { title: "PostgreSQL", img: SiPostgresql },
-  { title: "SQlite", img: SiSqlite},
-  { title: "insomnia", img: SiInsomnia},
+  { title: "SQlite", img: SiSqlite },
+  { title: "insomnia", img: SiInsomnia },
 ];
